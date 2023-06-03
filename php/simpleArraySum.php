f28ab021e94ee918f86a9d930a42459ed45c0ffe
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-06-03 10:16:07 -0300

    first commit

---
+++ php/simpleArraySum.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+ 
+
+function simpleArraySum() {
+ $ar = array(1, 2, 3, 5, 11, 9);
+    return array_sum($ar);
+
+}
+
+echo simpleArraySum();
+
+?>
